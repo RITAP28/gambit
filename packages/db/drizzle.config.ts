ef0f573ad84c1494ab49a261ad0cae1bd258bb9b
RITAP28 <ritap2804manutd@gmail.com>
2026-02-25 08:28:18 +0530

    initialised drizzle

---
+++ packages/db/drizzle.config.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import type { Config } from 'drizzle-kit'
+
+export default {
+    out: "./drizzle",
+    schema: "./src/schema",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!
+    }
+} satisfies Config
---
+++ packages/db/src/schema/user.ts
@@ -0,0 +1,7 @@
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    createdAt: timestamp("created_at").defaultNow().notNull()
+});
